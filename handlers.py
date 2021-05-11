a2db925aa3d2fe0dc33a53b4d7a729818381b220
Igor Vlasov <>
2021-05-11 16:20:32 +0000

    Прописали основные команды

--- handlers.py
+++ handlers.py
@@ -1,8 +1,33 @@
 from app import bot, dp
-from aiogram import Message
+from aiogram.types import Message
 from config import admin_id, todo, HELP
 import time
 
 
+
 async def send_to_admin(dp):
     await bot.send_message(chat_id = admin_id, text = "Бот запущен!")
+
+@dp.message_handler(command=["start"])
+async def start(message:Message):
+    await message.answer("Работает")
+
+@dp.message_handler(command=["add"])
+async def add(message:Message):
+    await message.answer("Работает")
+
+@dp.message_handler(command=["show"])
+async def show(message:Message):
+    await message.answer("Работает")
+
+@dp.message_handler(command=["done"])
+async def done(message:Message):
+    await message.answer("Работает")
+
+@dp.message_handler(command=["help"])
+async def help(message:Message):
+    await message.answer(HELP)
+
+@dp.message_handler()
+async def text(message:Message):
+    await message.answer(text = message.text)
